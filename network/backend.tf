daf428f728da22b6ad038ad923a1c78bb20fbb38
Dmitriy-py <dik.demon@mail.ru>
2026-05-03 17:53:07 +0300

    Fix: backend configuration and keys updated

--- network/backend.tf
+++ network/backend.tf
@@ -5,11 +5,14 @@ terraform {
     }
     bucket = "diploma-tfstate-klimovdg"
     region = "ru-central1"
-    key    = "terraform.tfstate"
+    key    = "network/terraform.tfstate" 
 
+    # Эти параметры отключают проверки, специфичные для AWS, 
+    # которые не работают с Yandex Cloud
     skip_region_validation      = true
     skip_credentials_validation = true
     skip_requesting_account_id  = true
     skip_s3_checksum            = true
+    use_path_style              = true 
   }
 }
